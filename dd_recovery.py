35e5c78f779b7d61e25652020cd271fd3e89eab7
Derek Lee <derek.lee@quadrent.co.nz>
2025-03-05 12:04:44 +1300

    feat: allow results to be captured and stored

--- dd_recovery.py
+++ dd_recovery.py
@@ -2,16 +2,18 @@ import argparse
 import subprocess
 import os
 import shutil
+from uuid import uuid4
 from geopy.geocoders import Nominatim
 from exif import Image
 from prettytable import PrettyTable
 
 class DDRecovery:
-    def __init__(self, filename, required_info):
+    def __init__(self, filename, required_info,task_id):
         self.filename = filename
         self.required_info = required_info
+        self.task_id = task_id
         self.table = self.setup_table(required_info)
-        self.output_dir = "output"
+        self.output_dir = f"results/{self.task_id}"
 
     def setup_table(self, required_info):
         table = PrettyTable()
@@ -21,27 +23,26 @@ class DDRecovery:
 
     def recover_dd(self):
         # Run subprocess to extract the dd via foremost
-        print(f"Performing dd extract on {self.filename}")
-        if os.path.exists(self.output_dir):
-            shutil.rmtree(self.output_dir)
-        result = subprocess.run(["foremost", self.filename,], capture_output=True, text=True)
+        print(f"Performing dd extract on {self.filename} to {self.output_dir}")
+        if not os.path.exists(self.output_dir):
+            os.makedirs(self.output_dir)
+        result = subprocess.run(["foremost", "-o", self.output_dir, self.filename], capture_output=True, text=True)
         print(f"Result: {result.stdout}")
 
         if result.stdout is None:
             print("Extraction successful")
 
     def process_files(self):
-        dir = "output"
-        folders = os.listdir(dir)
+        folders = os.listdir(self.output_dir)
 
         for folder in folders:
             if folder == "audit.txt":
                 continue
 
-            files = os.listdir(f"{dir}/{folder}")
+            files = os.listdir(f"{self.output_dir}/{folder}")
             for file in files:
                 print(f"Processing file: ({file}) from {folder} folder")
-                file_dir = f"{dir}/{folder}/{file}"
+                file_dir = f"{self.output_dir}/{folder}/{file}"
 
                 if self.required_info == "exif":
                     self.extract_exif(file_dir, file)
@@ -85,7 +86,8 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--filename", help="File Name with paths")
     parser.add_argument("--requiredInfo", help="Required type info to extract")
+    parser.add_argument("--task_id", help="Task ID",default=str(uuid4()))
     args = parser.parse_args()
 
-    processor = DDRecovery(args.filename, args.requiredInfo)
+    processor = DDRecovery(args.filename, args.requiredInfo, args.task_id)
     processor.run()
